dd53a080a0c604f6c0a2bcba8c166a320e6ad61f
Hassan <hassan.ali@g.austincc.edu>
2019-03-06 14:05:30 -0600

    Updates

---
+++ precache-manifest.1a6ef9ed1deba7a84483611dd8aaad17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab14a012a3ebdd80f5f",
+    "url": "/BookFinder/static/js/runtime~main.9ab14a01.js"
+  },
+  {
+    "revision": "d199ec33119c19fab120",
+    "url": "/BookFinder/static/js/main.d199ec33.chunk.js"
+  },
+  {
+    "revision": "43ed88e01f78e9bb495e",
+    "url": "/BookFinder/static/js/2.43ed88e0.chunk.js"
+  },
+  {
+    "revision": "d199ec33119c19fab120",
+    "url": "/BookFinder/static/css/main.0775d214.chunk.css"
+  },
+  {
+    "revision": "43ed88e01f78e9bb495e",
+    "url": "/BookFinder/static/css/2.5ebae1ac.chunk.css"
+  },
+  {
+    "revision": "e2671ff05159fc8e818fb181cd22d8f5",
+    "url": "/BookFinder/index.html"
+  }
+];
